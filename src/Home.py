be9bc0bf5fbff70b061cff1ce555ccf9bd526517
janylhl <jany.lahlouh@outlook.fr>
2024-02-17 10:37:19 +0100

    Multipage app

---
+++ src/Home.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.write("# NLP and LLM's personnal work demos ! 👋")
+
+st.sidebar.success("Select a demo above.")
+
+st.markdown(
+    """
+    Welcome to my streamlit app decated to NLP and LLM's personnal work demos !
+    Description comming soon !
+    """
+)
