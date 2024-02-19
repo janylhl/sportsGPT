1ceb2ed58a631d922e64386ea87564ba0eb74f01
janylhl <jany.lahlouh@outlook.fr>
2024-02-19 18:47:09 +0100

    training code update

--- src/llm/dialoGPT_finetuning.py
+++ src/llm/dialoGPT_finetuning.py
@@ -15,56 +15,10 @@ print('Chargement du dataset')
 # Chargement du dataset
 raw_datasets = load_dataset("OpenLLM-France/Claire-Dialogue-French-0.1")
 
-# Imprimer des échantillons
-#for input, target in zip(val_inputs[:5], val_targets[:5]):  # Imprimer les 5 premières paires
-#    print("Input:", input)
-#    print("Target:", target)
-#    print("---")
-
-
-
+# Load custom generator for french DialoGPT
 tokenizer = AutoTokenizer.from_pretrained('.models/dialoGPT-fr-tokenizer')
 tokenizer.pad_token = tokenizer.eos_token
-# TODO : Cette façon de faire fait exploser ma mémoire il faut la revoir et uriliser au maximum les outils HF
-def batch_tokenize_pairs(inputs, targets, batch_size=10):
-    tokenized_inputs = []
-    tokenized_targets = []
-    
-    for i in range(0, len(inputs), batch_size):
-        try:
-            batch_inputs = inputs[i:i+batch_size]
-            batch_targets = targets[i:i+batch_size]
-            batch_tokenized_inputs = tokenizer(batch_inputs, padding=True, truncation=True, max_length=512, return_tensors="tf")
-            batch_tokenized_targets = tokenizer(batch_targets, padding=True, truncation=True, max_length=512, return_tensors="tf")
-            
-            # Supposons que vous voulez juste stocker les input_ids pour simplifier
-            tokenized_inputs.extend(batch_tokenized_inputs['input_ids'])
-            tokenized_targets.extend(batch_tokenized_targets['input_ids'])
-            gc.collect()
-        except Exception as e:
-            print(f"Erreur lors de la tokenisation du lot {i}-{i+batch_size}: {e}")
-            break  # ou continue, selon si vous voulez arrêter au premier échec ou essayer de continuer
 
-    return tokenized_inputs, tokenized_targets
-
-def create_tf_dataset(tokenized_inputs, tokenized_targets):
-    # Extraire input_ids et attention_mask des inputs tokenisés
-    input_ids = tokenized_inputs['input_ids']
-    attention_mask = tokenized_inputs['attention_mask']
-    
-    # Les labels sont les input_ids des targets tokenisés
-    labels = tokenized_targets['input_ids']
-    
-    # Créer un tf.data.Dataset à partir des tensors
-    dataset = tf.data.Dataset.from_tensor_slices(({
-        "input_ids": input_ids,
-        "attention_mask": attention_mask,
-    }, labels))
-    
-    # Mélanger le dataset et le diviser en lots
-    dataset = dataset.shuffle(len(input_ids)).batch(8)
-    
-    return dataset
 
 def data_generator(inputs, targets, batch_size):
     for i in range(0, len(inputs), batch_size):
@@ -118,21 +72,33 @@ from transformers import TFAutoModelForCausalLM
 
 model = TFAutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-small")
 print("Entrainement")
-# Compilation du modèle
-optimizer = tf.keras.optimizers.Adam(learning_rate=5e-5)
-loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-model.compile(optimizer=optimizer, loss=loss)
 
-# Entraînement
+
+batch_size = 8
+num_epochs = 3
+# The number of training steps is the number of samples in the dataset, divided by the batch size then multiplied
+# by the total number of epochs. Note that the tf_train_dataset here is a batched tf.data.Dataset,
+# not the original Hugging Face Dataset, so its len() is already num_samples // batch_size.
+num_train_steps = len(train_dataset) * num_epochs
+lr_scheduler = tf.keras.optimizers.schedules.PolynomialDecay(initial_learning_rate=5e-5, end_learning_rate=0.0, decay_steps=num_train_steps)
+opt = tf.keras.optimizers.Adam(learning_rate=lr_scheduler)
+loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+model.compile(optimizer=opt, loss=loss, metrics=["accuracy"])
 model.fit(train_dataset, validation_data=val_dataset, epochs=3)
 
 
 
-from transformers import TFConversationalPipeline
+import evaluate
+import numpy as np
+preds = model.predict(val_dataset)["logits"]
+class_preds = np.argmax(preds, axis=1)
+metric = evaluate.load("glue", "mrpc")
+metric.compute(predictions=class_preds, references=raw_datasets["validation"]["label"])
 
+
+from transformers import TFConversationalPipeline
 # Création d'un pipeline conversationnel
 chatbot = TFConversationalPipeline(model=model, tokenizer=tokenizer)
-
 # Générer une réponse
 inputs = tokenizer.encode("Bonjour, comment ça va ?", return_tensors='tf')
 outputs = model.generate(inputs, max_length=50)
--- src/main.py
+++ src/main.py
@@ -1,3 +1,9 @@
 """
 Main script of the repo
 """
+
+import tensorflow as tf
+
+
+# Checking GPU availability
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
